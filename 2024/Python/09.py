8c72d1d5ffea2cffe83bc09cfe3483ae72da8005
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2024-12-09 16:58:45 -0500

    Completed 2024 Day 9 Part 2

--- 2024/Python/09.py
+++ 2024/Python/09.py
@@ -42,25 +42,34 @@ print(f"Part 1: {total}")
 disk = build_disk()
 file_index = len(disk) - 1
 file_id = disk[file_index]
-while file_id > 0:
+while file_id > 10:
     file_size = 1
     read_head = file_index - 1
     while disk[read_head] == disk[file_index]:
         file_size += 1
         read_head -= 1
-    try:
-        new_location = disk.index("." * file_size)
+    max_gap = 0
+    max_index = -1
+    current_gap = 0
+    for i in range(0, file_index):
+        if disk[i] == ".":
+            current_gap += 1
+        else:
+            if current_gap > max_gap:
+                max_gap = current_gap
+                max_index = i - current_gap
+            if max_gap >= file_size:
+                break
+            current_gap = 0
+    if max_index > -1 and max_gap >= file_size:
         for i in range(file_size):
-            disk[new_location + i] = file_id
-        for i in range(file_size):
+            disk[max_index + i] = file_id
             disk[file_index - i] = "."
-    except:
-        pass
     file_index -= file_size
     file_id -= 1
     while disk[file_index] != file_id:
         file_index -= 1
-print(disk)
+
 total = 0
 for index, num in enumerate(disk):
     if num == ".":
