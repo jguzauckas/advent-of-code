fb4cd3e97e84fa70b4b836fb5e2dbd84f4760674
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2025-12-02 07:06:40 -0500

    Completed 2025 Day 2

---
+++ 2025/Python/02.py
@@ -0,0 +1,36 @@
+# Read lines from file
+str_inp = []
+with open('2025/Inputs/02.txt', 'r') as inp:
+    str_inp = inp.readline()
+
+# Split file contents into list of ranges as strings
+list_inp = str_inp.split(",")
+
+# Split each entry into a first and last range value
+from collections import namedtuple
+idrange = namedtuple("idrange", ("first", "last"))
+idranges = []
+for str_in in list_inp:
+    current_range = str_in.split("-")
+    idranges.append(idrange(int(current_range[0]), int(current_range[1])))
+
+# Part 1
+invalid_ids_1 = []
+invalid_ids_2 = []
+for idrange in idranges:
+    for i in range(idrange.first, idrange.last + 1):
+        num_as_str = str(i)
+        if len(num_as_str) % 2 == 0 and num_as_str[:len(num_as_str)//2] == num_as_str[len(num_as_str)//2:]:
+            invalid_ids_1.append(i)
+        for j in range(1, len(num_as_str)):
+            if len(num_as_str) % j == 0:
+                str_to_check = num_as_str[:j]
+                flag = True
+                for k in range(0,len(num_as_str),j):
+                    if str_to_check != num_as_str[k:k+j]:
+                        flag = False
+                if flag:
+                    invalid_ids_2.append(i)
+
+print(sum(invalid_ids_1), sum(set(invalid_ids_2)))
+
