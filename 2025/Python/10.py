e5a74196bb629bafa5709b7ea9660515a1ba7e66
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2025-12-10 12:45:44 -0500

    Finish Day 10 Part 1, Start Part 2

--- 2025/Python/10.py
+++ 2025/Python/10.py
@@ -9,46 +9,71 @@ for row in list_inp:
     lights.append([True if x == "#" else False for x in temp[0][1:-1]])
     buttons.append([[int(x) for x in y[1:-1].split(',')] for y in temp[1:-1]])
     joltages.append([int(x) for x in temp[-1][1:-1].split(',')])
-
-light_key = lights[0]
-button_options = buttons[0]
-results = []
-for option in button_options:
-    lights = [False for _ in range(len(light_key))]
-    score = 0
-    for light in option:
-        lights[light] = not lights[light]
-    for light, check in zip(lights, light_key):
-        if light == check:
-            score += 1
-    results.append((score, [option], lights))
-results.sort()
-results.reverse()
-print(results)
-for result in results:
-    score_to_beat, options, current_lights = result
-    new_results = []
-    flag = False
+answer = 0
+for index in range(len(list_inp)):
+    light_key = lights[index]
+    button_options = buttons[index]
+    results = []
+    solution = 0
     for option in button_options:
-        lights = current_lights[:]
+        row_lights = [False for _ in range(len(light_key))]
         score = 0
         for light in option:
-            lights[light] = not lights[light]
-        for light, check in zip(lights, light_key):
+            row_lights[light] = not row_lights[light]
+        for light, check in zip(row_lights, light_key):
             if light == check:
                 score += 1
-        if score >= score_to_beat:
-            flag = True
-        new_results.append((score, options + [option], lights))
         if score == len(light_key):
-            solution = len(options) + 1
-            print(solution)
+            solution = 1
             break
-    if flag:
-        results.extend(new_results)
-    if score > 0:
-        break
-    
-results.sort()
-results.reverse()
-print(results)
+        results.append((score, [option], row_lights))
+    results.sort()
+    results.reverse()
+    while(solution == 0):
+        results_keep = []
+        for result in results:
+            score_to_beat, options, current_row_lights = result
+            new_results = []
+            flag = False
+            for option in button_options:
+                if option not in options:
+                    row_lights = current_row_lights[:]
+                    score = 0
+                    for light in option:
+                        row_lights[light] = not row_lights[light]
+                    for light, check in zip(row_lights, light_key):
+                        if light == check:
+                            score += 1
+                    if score >= score_to_beat:
+                        flag = True
+                    new_results.append((score, options + [option], row_lights))
+                    if score == len(light_key):
+                        solution = len(options) + 1
+                        break
+            if flag:
+                results_keep.extend(new_results)
+            if solution > 0:
+                break
+        results = results_keep[:]
+        results.sort()
+        results.reverse()
+    print(index, ":", solution)
+    answer += solution
+print(answer)
+
+for button, joltage in zip(buttons, joltages):
+    matrix = []
+    for i in range(len(joltage)):
+        row = []
+        for option in button:
+            if i in option:
+                row.append(1)
+            else:
+                row.append(0)
+        row.append(joltage[i])
+        matrix.append(row)
+    leader = -1
+    index = 0
+    while leader < 0:
+        for i in range(len(matrix)):
+            if 
